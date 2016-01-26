5d99bac1a66cb55d92c31ae3036d85678571dd8d
Clayton Petty <git.notyalc@gmail.com>
2016-01-25 20:43:27 -0600

    initial

---
+++ MovieViewer/MovieViewer/MovieViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MovieViewController.swift
+//  MovieViewer
+//
+//  Created by Clayton Petty on 1/25/16.
+//  Copyright © 2016 Clayton Petty. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
